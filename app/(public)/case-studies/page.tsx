fb8e928e524ea3aaf4500b08d047c0b8eb2739f8
Najmal Fawaz <mp.najmalfawaz@gmail.com>
2026-02-25 14:44:57 +0400

    fix(case-studies): correct getCollection property from caseStudies to case-studies to properly render listing and details pages from static data

--- app/(public)/case-studies/page.tsx
+++ app/(public)/case-studies/page.tsx
@@ -3,7 +3,7 @@ import CaseStudiesList from '@/components/case-studies/case-studies-list';
 import { getCollection } from '@/lib/db';
 
 export default async function CaseStudies() {
-  const caseStudies = await getCollection('caseStudies');
+  const caseStudies = await getCollection('case-studies');
 
   return (
     <main>
