8f534e206ba0afd7c4c570dffcfe0168954f36e6
Najmal Fawaz <mp.najmalfawaz@gmail.com>
2026-04-23 04:21:54 +0400

    chore: remove redundant diagnostic and one-time migration scripts

--- scripts/check-blog.js
+++
@@ -1,9 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-
-async function main() {
-    const blog = await prisma.blog.findFirst();
-    console.log(blog.content);
-}
-
-main().catch(console.error).finally(() => prisma.$disconnect());
